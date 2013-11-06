c67b21ac575d9f250183767abbd74f2d00e97c1e
alex-pex <alexandre.paixao@stadline.com>
2013-11-06 17:25:34 +0100

    Renamed the story creation event

--- Resources/public/js/storyjs-embed.js
+++ Resources/public/js/storyjs-embed.js
@@ -800,7 +800,7 @@ function createStoryJS(c, src) {
 		VMM.debug = storyjs_e_config.debug;
 		storyjs_embedjs = new VMM.Timeline(storyjs_e_config.id);
 		storyjs_embedjs.init(storyjs_e_config);
-        VMM.fireEvent(global, 'LOADED', storyjs_embedjs);
+        VMM.fireEvent(global, 'STORYCREATED', storyjs_embedjs);
 		if (isCDN) {
 			VMM.bindEvent(global, onHeadline, "HEADLINE");
 		}
